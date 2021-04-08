066f35e0fab9a193dca226ce228349a96e606b1f
Victor Menezes <victormenezez15@gmail.com>
2021-04-08 20:35:10 -0300

    Using Styled Components

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { Title } from './styles';
+
+const Dashboard: React.FC = () => {
+  return <Title>Explore qmwfpsom aspmoda</Title>;
+};
+
+export default Dashboard;
